dbd6b81e32fcf174f605f3f491d50d4191212554
fathurohmansf <fathurrhm909@gmail.com>
2024-08-21 10:45:23 +0700

    add to fix

--- lib/screen/third_screen.dart
+++ lib/screen/third_screen.dart
@@ -1,9 +1,9 @@
 import 'package:flutter/material.dart';
+import 'package:number_paginator/number_paginator.dart';
 import 'package:provider/provider.dart';
 import 'package:suitmedia_testcode/provider/name_provider.dart';
 import 'package:http/http.dart' as http;
 import 'dart:convert';
-import 'package:flutter/scheduler.dart';
 
 class ThirdScreen extends StatefulWidget {
   const ThirdScreen({super.key});
@@ -14,9 +14,9 @@ class ThirdScreen extends StatefulWidget {
 
 class _ThirdScreenState extends State<ThirdScreen> {
   List<dynamic> users = [];
-  int currentPage = 1;
+  int page = 1;
+  int totalPages = 10;
   bool isLoading = false;
-  bool isLastPage = false;
   bool isEmpty = false;
 
   @override
@@ -32,31 +32,25 @@ class _ThirdScreenState extends State<ThirdScreen> {
       isLoading = true;
     });
 
-    if (isRefresh) {
-      currentPage = 1;
-      isLastPage = false;
-      users.clear();
-    }
-
     final response = await http.get(
-        Uri.parse('https://reqres.in/api/users?page=$currentPage&per_page=10'));
+      Uri.parse('https://reqres.in/api/users?page=$page&per_page=10'),
+    );
 
     if (response.statusCode == 200) {
       final data = json.decode(response.body);
-      if (data['data'].isEmpty) {
-        setState(() {
-          isEmpty = true;
-        });
-      } else {
-        setState(() {
-          users.addAll(data['data']);
-          currentPage++;
-          if (data['data'].length < 10) {
-            isLastPage = true;
-          }
-          isEmpty = false;
-        });
-      }
+      final List<dynamic> newUsers = data['data'];
+      totalPages = data['total_pages'];
+
+      setState(() {
+        if (isRefresh) {
+          users = newUsers;
+        } else {
+          users.addAll(newUsers);
+        }
+        isEmpty = users.isEmpty;
+      });
+    } else {
+      // Handle errors
     }
 
     setState(() {
@@ -73,34 +67,40 @@ class _ThirdScreenState extends State<ThirdScreen> {
       ),
       body: isEmpty
           ? const Center(child: Text('No Users Available'))
-          : RefreshIndicator(
-              onRefresh: () => fetchUsers(isRefresh: true),
-              child: ListView.builder(
-                itemCount: isLastPage ? users.length : users.length + 1,
-                itemBuilder: (context, index) {
-                  if (index == users.length) {
-                    // untuk membuat callback fetchuser
-                    SchedulerBinding.instance.addPostFrameCallback((_) {
-                      fetchUsers();
-                    });
-                    return const Center(child: CircularProgressIndicator());
-                  }
-
-                  final user = users[index];
-                  return ListTile(
-                    leading: CircleAvatar(
-                      backgroundImage: NetworkImage(user['avatar']),
-                    ),
-                    title: Text('${user['first_name']} ${user['last_name']}'),
-                    subtitle: Text(user['email']),
-                    onTap: () {
-                      Provider.of<NameProvider>(context, listen: false).setName(
-                          '${user['first_name']} ${user['last_name']}');
-                      Navigator.pop(context);
+          : Column(
+              children: [
+                Expanded(
+                  child: ListView.builder(
+                    itemCount: users.length,
+                    itemBuilder: (context, index) {
+                      final user = users[index];
+                      return ListTile(
+                        leading: CircleAvatar(
+                          backgroundImage: NetworkImage(user['avatar']),
+                        ),
+                        title:
+                            Text('${user['first_name']} ${user['last_name']}'),
+                        subtitle: Text(user['email']),
+                        onTap: () {
+                          Provider.of<NameProvider>(context, listen: false)
+                              .setName(
+                                  '${user['first_name']} ${user['last_name']}');
+                          Navigator.pop(context);
+                        },
+                      );
                     },
-                  );
-                },
-              ),
+                  ),
+                ),
+                NumberPaginator(
+                  numberPages: totalPages,
+                  onPageChange: (index) {
+                    setState(() {
+                      page = index + 1;
+                      fetchUsers(isRefresh: true);
+                    });
+                  },
+                ),
+              ],
             ),
     );
   }
